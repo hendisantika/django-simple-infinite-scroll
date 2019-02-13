e64053001478b3c9dab1321f84546c1f439a3c24
hendisantika <hendisantika@yahoo.co.id>
2019-02-14 06:09:03 +0700

    #10 - Run python manage.py makemigrations

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 
-from mysite.blog import views
+from blog import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
