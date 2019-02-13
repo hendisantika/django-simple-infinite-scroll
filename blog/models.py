98b1166c91109617ce9880278459be847821801c
hendisantika <hendisantika@yahoo.co.id>
2019-02-14 06:02:30 +0700

    #3 - Add Article models

--- blog/models.py
+++ blog/models.py
@@ -1,1 +1,9 @@
 # Create your models here.
+from django.db import models
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=30)
+    body = models.TextField(max_length=2000)
+    date = models.DateTimeField()
+    author = models.CharField(max_length=30)
